837f7c397825da388c263e46d67ed1d949a12393
Peter Bethell <moodelymoo@gmail.com>
2021-02-09 20:05:01 +0000

    Initial planning of game loop in the game state controller

--- src/Scrabble/GameRuntime/GameStateController.java
+++ src/Scrabble/GameRuntime/GameStateController.java
@@ -3,6 +3,30 @@ package Scrabble.GameRuntime;
 public class GameStateController {
 
     public GameStateController() {
+
+        // setup
+
+        // assume default setup worked as intended
+
+        // game loop (round)
+            // check no prior challenge turn skip applied
+
+            // p1 turn
+            // must play tiles
+
+            // p2 can challenge
+
+                // if no challenge
+                    // refill hand
+
+                // else check word
+                    //if word exists
+                        // skip p2 turn
+
+                    // else
+                        // remove tiles from board to p1 hand
+
+        // go to round start and increment player e.g. p1 -> p2 ect...
     }
 
 
